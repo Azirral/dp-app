2b0e21fc88a428e19b6c818d2ea3f4e2e0aafa01
Kamil Lesiński <s189501@student.pg.edu.pl>
2024-06-04 18:24:42 +0200

    printing possible options

--- client.py
+++ client.py
@@ -4,6 +4,10 @@ def start_client(host='localhost', port=65432):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         print(f'Connected to server at {host}:{port}')
+        print("available categories:\n"
+              " ones: None\n twos: None\n threes: None\n fours: None\n fives: None\n sixes: None\n"
+              " three_of_a_kind: None\n four_of_a_kind: None\n full_house: None\n small_straight: None\n"
+              " large_straight: None\n yahtzee: None\n chance: None\n")
         while True:
             message = input('Enter command (roll, keep <indices>, score <category>, add <category>, reset, exit): ')
             if message.lower() == 'exit':
